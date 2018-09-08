efee8c7639f378508dc1408ff5c6d0d182c66f24
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-09-07 23:37:23 -0400

    Markers

---
+++ src/Markers.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+class Markers extends Components {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            'thingsToDo': [
+                {
+                   'name': "Caroowinds",
+                   'lat': "35.1017389",
+                   'lng': "-80.9502442",
+                   'address': "300 Carowinds Boulevard, Charlotte, NC 28273"
+                },
+                {
+                    'name': "U.S. National Whitewater Center",
+                    'lat': "35.2805738",
+                    'lng': "-80.9891828",
+                    'address': "5000 Whitewater Center Pkwy, Charlotte, NC 28214" 
+                },
+                {
+                    'name': "Discovery Place Science",
+                    'lat': "35.2292981",
+                    'lng': "-80.8429949",
+                    'address': "301 N Tryon St, Charlotte, NC 28202"  
+                },
+                {
+                    'name': "Nascar Hall of Fame",
+                    'lat': "35.2213247",
+                    'lng': "-80.8455966",
+                    'address': "400 E M.L.K. Jr Blvd, Charlotte, NC 28202"  
+                },
+                {
+                    'name': "Daniel Stowe Botanical Garden",
+                    'lat': "35.1682438",
+                    'lng': "-81.0585699",
+                    'address': "6500 S New Hope Rd, Belmont, NC 28012" 
+                },
+                {
+                    'name': "Carolinas Aviation Museum",
+                    'lat': "35.219796",
+                    'lng': "-80.9322797",
+                    'address': "4672 1st Flight Dr, Charlotte, NC 28208" 
+                },
+                {
+                    'name': "Sea Life Charlotte-Concord Aquarium",
+                    'lat': "35.3700689",
+                    'lng': "-80.7261185",
+                    'address': "Concord Mills Mall, 8111 Concord Mills Boulevard, Concord, NC 28027" 
+                },
+                {
+                    'name': "Billy Graham Library",
+                    'lat': "35.1903725",
+                    'lng': "-80.9114693",
+                    'address': "4330 Westmont Dr, Charlotte, NC 28217" 
+                },
+                {
+                    'name': "Levine Museum of the New South",
+                    'lat': "35.2281189",
+                    'lng': "-80.840919",
+                    'address': "200 E 7th St, Charlotte, NC 28202" 
+                },
+                {
+                    'name': "Historic Latta Plantation",
+                    'lat': "35.3535336",
+                    'lng': "-80.931879",
+                    'address': "5225 Sample Rd, Huntersville, NC 28078" 
+                }
+            ]
+    }
+    }
+    
+    render(){
+        return()
+    }
+}
+
+export default Markers;
