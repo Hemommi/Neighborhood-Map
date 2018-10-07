5f8e86a1393ef552e72312dde5f32e8c2fe56619
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-10-07 16:16:18 -0400

    Venues

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import scriptLoader from 'react-async-script-loader';
-import 'whatwg-fetch';
+// import 'whatwg-fetch';
+import axios from'axios';
 
 class Map extends Component {
     //super- access the constructor method of the parent class. Props is included to access this.props inside of constructor.//
@@ -12,21 +13,32 @@ class Map extends Component {
     }
 
     //*Foursquare*//
-     componentDidMount(){
-         const endpoint = 'https://api.foursquare.com/v2/venues/explore';
+    componentDidMount(){
+        this.getVenues(); 
+    }
 
-         const parameters = {
-             client_id: "2RQ0EBUNQGMO32XYW5FECM1DPGIXMAXY1AXBSN2LHM1PZFB5",
-             client_secret: "04EIRAYHI2QHP4KJ3RWMSDBCSS3ERDF0ROS0BHYT4GT0WCC0",
-             query: "things to do",
-             near: "Charlotte, NC",
-             limit: 10
-         };
-         //*URLSearchParams method allows build query parameters using objects*//
-         fetch(endpoint + new URLSearchParams(parameters), {
-             method: "GET"
-         }).then();
-     }
+    //*getVenues method*//
+    getVenues() {
+
+        const endpoint = 'https://api.foursquare.com/v2/venues/explore';
+
+        //*V parameter added for API changes up to this date*//
+        const myParameters = {
+            clientId: "2RQ0EBUNQGMO32XYW5FECM1DPGIXMAXY1AXBSN2LHM1PZFB5",
+            clientSecret: "04EIRAYHI2QHP4KJ3RWMSDBCSS3ERDF0ROS0BHYT4GT0WCC0",
+            query: "things to do",
+            near: "Charlotte, NC",
+            v: "20181007"
+        };
+        //*URLSearchParams method allows build query parameters using objects*//
+        //*axios- promise-based*//
+        axios.get(endpoint + new URLSearchParams(myParameters)).then(response => {
+           console.log(response);
+        })
+        .catch(error => {
+            console.log("Error" + error);
+        });
+    }
 
     //*A decorator for script lazy loading on react component.*//
     componentWillReceiveProps({isScriptLoaded, isScriptLoadSucceed}) {
